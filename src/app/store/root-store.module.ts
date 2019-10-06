e5f2a8e2c52fcbf01dbc135033fe1497495ccc99
Wataru.Kasahara <kasahara1019@gmail.com>
2019-10-06 15:42:41 +0900

    StoreModule の root 設定

--- src/app/store/root-store.module.ts
+++ src/app/store/root-store.module.ts
@@ -1,10 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { StoreModule } from '@ngrx/store';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule,
-  ],
+  imports: [CommonModule, StoreModule.forRoot({})],
 })
-export class RootStoreModule { }
+export class RootStoreModule {}
