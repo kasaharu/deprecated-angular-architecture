2e58a14afa1f45135670454164a3c70b8f2ea125
Wataru.Kasahara <kasahara1019@gmail.com>
2019-10-06 15:50:26 +0900

    yarn ng g module store/hero-store --module store/root-store.module.ts --lint-fix

--- src/app/store/root-store.module.ts
+++ src/app/store/root-store.module.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { StoreModule } from '@ngrx/store';
+import { HeroStoreModule } from './hero-store/hero-store.module';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, StoreModule.forRoot({})],
+  imports: [CommonModule, StoreModule.forRoot({}), HeroStoreModule],
 })
 export class RootStoreModule {}
