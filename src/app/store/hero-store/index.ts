7bfea7b9230e280472aff8c9f1658a84dc0fd8d0
Wataru.Kasahara <kasahara1019@gmail.com>
2019-10-08 09:43:23 +0900

    HeroStore に hero 追加時の action, reducer を実装

--- src/app/store/hero-store/index.ts
+++ src/app/store/hero-store/index.ts
@@ -13,14 +13,19 @@ export const initialState: State = {
 
 // NOTE: Actions
 export const saveHeroes = createAction('[Hero] Save heroes', props<{ heroes: Hero[] }>());
+export const addHero = createAction('[Hero] Add hero', props<{ hero: Hero }>());
 // tslint:disable-next-line: variable-name
-export const Actions = { saveHeroes };
+export const Actions = { saveHeroes, addHero };
 // tslint:disable-next-line: variable-name
 const ActionsUnion = union(Actions);
 type ActionsUnionType = typeof ActionsUnion;
 
 // NOTE: Reducer
-const heroReducer = createReducer(initialState, on(saveHeroes, (state, { heroes }) => ({ ...state, heroes })));
+const heroReducer = createReducer(
+  initialState,
+  on(saveHeroes, (state, { heroes }) => ({ ...state, heroes })),
+  on(addHero, (state, { hero }) => ({ ...state, heroes: [...state.heroes, hero] })),
+);
 
 export default function reducer(state: State, action: ActionsUnionType): State {
   return heroReducer(state, action);
