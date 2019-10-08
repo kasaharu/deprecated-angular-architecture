5c33c6c02d2cb35f720457c9172ce5af4da5378b
Wataru.Kasahara <kasahara1019@gmail.com>
2019-10-08 10:08:56 +0900

    dashboard 処理の置き換え

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
-import { Actions as HeroStoreActions } from '../store/hero-store';
+import { Actions as HeroStoreActions, selectStateFromTaskStore } from '../store/hero-store';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,7 +11,12 @@ import { Actions as HeroStoreActions } from '../store/hero-store';
   styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
-  heroes: Hero[] = [];
+  heroes$: Observable<Hero[]> = selectStateFromTaskStore(this.store$, (state) => {
+    if (state.heroes === null) {
+      return state.heroes;
+    }
+    return state.heroes.slice(1, 5);
+  });
 
   constructor(private store$: Store<{}>, private heroService: HeroService) {}
 
@@ -20,7 +26,6 @@ export class DashboardComponent implements OnInit {
 
   getHeroes(): void {
     this.heroService.getHeroes().subscribe((heroes) => {
-      this.heroes = heroes.slice(1, 5);
       this.store$.dispatch(HeroStoreActions.saveHeroes({ heroes }));
     });
   }
