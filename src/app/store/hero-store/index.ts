f6cf4f7ed37d8d879a44adb7646199a2035be97c
Wataru.Kasahara <kasahara1019@gmail.com>
2019-10-13 17:32:05 +0900

    選択した hero を管理するために store を更新

--- src/app/store/hero-store/index.ts
+++ src/app/store/hero-store/index.ts
@@ -5,19 +5,22 @@ import { createFeatureStoreSelector } from '../helpers/selector-helper';
 // NOTE: State
 export interface State {
   heroes: Hero[] | null;
+  selectedHero: Hero | null;
 }
 
 export const initialState: State = {
   heroes: null,
+  selectedHero: null,
 };
 
 // NOTE: Actions
 const saveHeroes = createAction('[Hero] Save heroes', props<{ heroes: Hero[] }>());
 const addHero = createAction('[Hero] Add hero', props<{ hero: Hero }>());
 const deleteHero = createAction('[Hero] Delete hero', props<{ hero: Hero }>());
+const selectHero = createAction('[Hero] Select hero', props<{ hero: Hero }>());
 
 // tslint:disable-next-line: variable-name
-export const Actions = { saveHeroes, addHero, deleteHero };
+export const Actions = { saveHeroes, addHero, deleteHero, selectHero };
 // tslint:disable-next-line: variable-name
 const ActionsUnion = union(Actions);
 type ActionsUnionType = typeof ActionsUnion;
@@ -28,6 +31,7 @@ const heroReducer = createReducer(
   on(saveHeroes, (state, { heroes }) => ({ ...state, heroes })),
   on(addHero, (state, { hero }) => ({ ...state, heroes: [...state.heroes, hero] })),
   on(deleteHero, (state, { hero }) => ({ ...state, heroes: state.heroes.filter((h) => h !== hero) })),
+  on(selectHero, (state, { hero }) => ({ ...state, selectedHero: hero })),
 );
 
 export default function reducer(state: State, action: ActionsUnionType): State {
