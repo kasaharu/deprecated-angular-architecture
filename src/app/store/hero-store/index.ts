905e72685704255634c7ccc7a8357b5e08f3101d
Wataru.Kasahara <kasahara1019@gmail.com>
2019-10-13 18:37:53 +0900

    検索にヒットした hero を管理するために store を更新

--- src/app/store/hero-store/index.ts
+++ src/app/store/hero-store/index.ts
@@ -7,12 +7,14 @@ export interface State {
   heroes: Hero[] | null;
   selectedHero: Hero | null;
   searchTerms: string;
+  searchedHeroes: Hero[] | null;
 }
 
 export const initialState: State = {
   heroes: null,
   selectedHero: null,
   searchTerms: '',
+  searchedHeroes: null,
 };
 
 // NOTE: Actions
@@ -21,9 +23,10 @@ const addHero = createAction('[Hero] Add hero', props<{ hero: Hero }>());
 const deleteHero = createAction('[Hero] Delete hero', props<{ hero: Hero }>());
 const selectHero = createAction('[Hero] Select hero', props<{ hero: Hero }>());
 const changeSearchTerms = createAction('[Hero] Change search terms', props<{ searchTerms: string }>());
+const saveSearchedHeroes = createAction('[Hero] Save searched heroes', props<{ heroes: Hero[] }>());
 
 // tslint:disable-next-line: variable-name
-export const Actions = { saveHeroes, addHero, deleteHero, selectHero, changeSearchTerms };
+export const Actions = { saveHeroes, addHero, deleteHero, selectHero, changeSearchTerms, saveSearchedHeroes };
 // tslint:disable-next-line: variable-name
 const ActionsUnion = union(Actions);
 type ActionsUnionType = typeof ActionsUnion;
@@ -36,6 +39,7 @@ const heroReducer = createReducer(
   on(deleteHero, (state, { hero }) => ({ ...state, heroes: state.heroes.filter((h) => h !== hero) })),
   on(selectHero, (state, { hero }) => ({ ...state, selectedHero: hero })),
   on(changeSearchTerms, (state, { searchTerms }) => ({ ...state, searchTerms })),
+  on(saveSearchedHeroes, (state, { heroes }) => ({ ...state, searchedHeroes: heroes })),
 );
 
 export default function reducer(state: State, action: ActionsUnionType): State {
